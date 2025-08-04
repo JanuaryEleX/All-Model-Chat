f37375265b981b7df0cb0d14ea13ffecec988af9
HY <58380076+JanuaryEleX@users.noreply.github.com>
2025-08-04 16:42:24 +0800

    Update sw.js

--- all-model-chat/sw.js
+++ all-model-chat/sw.js
@@ -7,7 +7,7 @@ try {
 
 const CACHE_NAME = 'all-model-chat-cache-v3';
 const API_HOSTS = ['veerrfntcryn.ap-southeast-1.clawcloudrun.com'];
-const TARGET_URL_PREFIX = 'https://veerrfntcryn.ap-southeast-1.clawcloudrun.com/proxy/gemini';
+const TARGET_URL_PREFIX = 'https://veerrfntcryn.ap-southeast-1.clawcloudrun.com/proxy/gemini/v1beta';
 const STATIC_APP_SHELL_URLS = ['/', '/index.html', '/favicon.png', '/manifest.json'];
 
 let proxyUrl = null;
